3214900acc528145e6c1c5ccaae8cdbcec41b397
HopenLau <lauhokfan@163.com>
2022-08-19 14:36:06 +0800

    use jakarta.servlet-api 6.0.0 and jakarta.servlet.jsp-api 3.1.0

--- src/main/java/com/flock/listener/TestListener.java
+++ src/main/java/com/flock/listener/TestListener.java
@@ -1,7 +1,7 @@
 package com.flock.listener;
 
-import javax.servlet.ServletContextListener;
-import javax.servlet.annotation.WebListener;
+import jakarta.servlet.ServletContextListener;
+import jakarta.servlet.annotation.WebListener;
 
 @WebListener("This is only a demo listener")
 public class TestListener implements ServletContextListener {
